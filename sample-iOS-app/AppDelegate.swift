4a8f4238c819088c79b2746412604d97d700e04f
hari-testpress <hari@testpress.in>
2023-06-09 18:15:05 +0530

    Use TPStreamsSDK to play streams video

--- sample-iOS-app/AppDelegate.swift
+++ sample-iOS-app/AppDelegate.swift
@@ -6,14 +6,12 @@
 //
 
 import UIKit
+import TPStreamsSDK
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        TPStreamsSDK.initialize(withOrgCode: "6eafqn")
         return true
     }
 
--- sample-iOS-app/ViewController.swift
+++ sample-iOS-app/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import SwiftUI
+import TPStreamsSDK
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var playerContainer: UIView!
+    let player = TPAVPlayer(assetID: "8eaHZjXt6km", accessToken: "16b608ba-9979-45a0-94fb-b27c1a86b3c1")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        playerContainer.removeFromSuperview()
+        
+        let playerView = UIHostingController(rootView: TPStreamPlayerView(player: player))
+        playerView.view.frame = playerContainer.frame
+        view.addSubview(playerView.view)
     }
-
-
+    
+    @IBAction func togglePlay(_ sender: UIButton) {
+        if (player.timeControlStatus == .playing){
+            player.pause()
+            sender.setTitle("Play", for: .normal)
+        } else {
+            player.play()
+            sender.setTitle("Pause", for: .normal)
+        }
+    }
 }
 
