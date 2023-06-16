99c9abcf5d8bac1d65e16777d4876a2f3eaf480a
hari-testpress <hari@testpress.in>
2023-06-16 07:33:41 +0530

    Update viewcontroller to handle fullscreen

--- sample-iOS-app/ViewController.swift
+++ sample-iOS-app/ViewController.swift
@@ -12,14 +12,28 @@ import TPStreamsSDK
 class ViewController: UIViewController {
     @IBOutlet weak var playerContainer: UIView!
     let player = TPAVPlayer(assetID: "8eaHZjXt6km", accessToken: "16b608ba-9979-45a0-94fb-b27c1a86b3c1")
-    
+    var hostingController: UIViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let playerView = UIHostingController(rootView: TPStreamPlayerView(player: player))
-        playerView.view.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 280)
-        view.addSubview(playerView.view)
+        setupHostingController()
+    }
+    
+    private func setupHostingController() {
+        hostingController = UIHostingController(rootView: TPStreamPlayerView(player: player))
+        hostingController!.view.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 280)
+        addChild(hostingController!)
+        view.addSubview(hostingController!.view)
+        hostingController!.didMove(toParent: self)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if UIDevice.current.orientation.isLandscape {
+            self.hostingController?.view.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        } else {
+            self.hostingController?.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 280)
+        }
     }
     
     @IBAction func togglePlay(_ sender: UIButton) {
